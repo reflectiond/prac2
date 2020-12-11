8aa8c9c85ba542be71883e631499e1e550f05493
reflectiond <mopo3ko_mod@mail.ru>
2020-12-11 16:34:33 +0500

    https://habr.com/ru/company/ruvds/blog/438986/

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Joke from './Joke.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Joke text={{
+        question:'What is a joke?',
+        punchLine:'It\'s a text',
+      }}/>
     </div>
   );
 }
