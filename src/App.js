3a4a2d4608c2540b3852dbda2d2064813c1e3b19
Ясинский Евгений Тимурович <@choke.lan>
2020-12-14 16:13:41 +0500

    added product component and injected rendering of all products in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-
+import products from './vschoolProducts'
+import Product from './Product.js'
 
 function App() {
 
-
+const product = products.map(
+  product => {
+    return(
+      <Product
+        key={product.id} 
+        content={{name:product.name, price: product.price, description: product.description}}
+      />
+    );
+  }
+)
   return (
     <div>
-
+      {product}
     </div>
   );
 }
