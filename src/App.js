c94eb4f5927f229700962211638be620c2a7b0e7
Ясинский Евгений Тимурович <@choke.lan>
2020-12-15 11:11:09 +0500

    cleared project for any task

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import Joke from './Joke.js'
-import jokesData from './JokesData'
+
 
 function App() {
-  const jokesComponents = jokesData.map(joke => 
-    <Joke key={joke.id} text={{question: joke.question, punchLine: joke.punchLine}}/>
-  );
+
   return (
     <div>
-      {jokesComponents}   
+    
     </div>
   );
 }
