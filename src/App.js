98604f38e8836e1a47d2e4e8574cc071a25ea915
Ясинский Евгений Тимурович <@choke.lan>
2020-12-15 09:40:30 +0500

    adding button to delete tickets

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import TodoItem from './TodoItem';
 import todosData from './todosData'
 
-const toDoData = todosData.map((item) => (
-  <TodoItem
-    key={item.id}
-    content={{ completed: item.completed, text: item.text }}
-  />
-));
+
 function App() {
+  function ClearItem(n) {
+    console.log(n);
+  }
+  const toDoData = todosData.map((item) => (
+    <TodoItem
+      key={item.id}
+      EventHandler={ClearItem(item.id)}
+      content={{ completed: item.completed, text: item.text }}
+    />
+  ));
   return (
     <div>
       {toDoData}
